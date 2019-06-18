b34874a2ab1dcc87688549a84b36f91a692b20bf
Tanvir Hossain Saad <saadtan@gmail.com>
2019-06-18 13:24:18 +0600

    add new anom icons

--- app/src/main/java/one/anom/wallet/SettingsActivity2.java
+++ app/src/main/java/one/anom/wallet/SettingsActivity2.java
@@ -895,7 +895,7 @@ public class SettingsActivity2 extends PreferenceActivity	{
                 });
 
                 Preference aboutPref = (Preference) findPreference("about");
-                aboutPref.setSummary("Samourai," + " " + getResources().getString(R.string.version_name));
+                aboutPref.setSummary("Anom," + " " + getResources().getString(R.string.version_name));
                 aboutPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                     public boolean onPreferenceClick(Preference preference) {
                         Intent intent = new Intent(SettingsActivity2.this, AboutActivity.class);
