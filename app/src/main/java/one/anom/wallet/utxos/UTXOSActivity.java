961ddb6c27fa163ae4d1967c5f282e5a762d6f55
Tanvir Hossain Saad <saadtan@gmail.com>
2020-06-02 11:52:16 +0600

    rename wallet to anom wallet

--- app/src/main/java/one/anom/wallet/utxos/UTXOSActivity.java
+++ app/src/main/java/one/anom/wallet/utxos/UTXOSActivity.java
@@ -149,7 +149,7 @@ public class UTXOSActivity extends SamouraiActivity implements ActionMode.Callba
         if (!APIFactory.getInstance(getApplicationContext())
                 .walletInit) {
             if (!shownWalletLoadingMessage) {
-                Snackbar.make(utxoList.getRootView(), "Please wait... your wallet is still loading ", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(utxoList.getRootView(), "Please wait... your anom wallet is still loading ", Snackbar.LENGTH_LONG).show();
                 shownWalletLoadingMessage = true;
             }
 
