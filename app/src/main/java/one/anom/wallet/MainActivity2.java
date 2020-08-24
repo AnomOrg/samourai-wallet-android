e1a8136e940d0ad3e6662a0e9357c506af21fd4c
Tanvir Hossain Saad <saadtan@gmail.com>
2020-08-24 14:33:27 +0600

    remove checking param of dojo

--- app/src/main/java/one/anom/wallet/MainActivity2.java
+++ app/src/main/java/one/anom/wallet/MainActivity2.java
@@ -137,11 +137,9 @@ public class MainActivity2 extends Activity {
         boolean hasRun = PrefsUtil.getInstance(this).getValue(PrefsUtil.IS_FIRST_TIME, false);
         if (!hasRun) {
             PrefsUtil.getInstance(getApplicationContext()).setValue(PrefsUtil.IS_FIRST_TIME, true);
-            if(DojoUtil.getInstance(getApplicationContext()).getDojoParams() != null ){
                 //remove dojo node on first run
                 DojoUtil.getInstance(getApplicationContext()).clear();
                 PrefsUtil.getInstance(this).setValue(PrefsUtil.ENABLE_TOR, false);
-            }
         }
 
 //        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
